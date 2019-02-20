7578d3509744fe9ce4aceb4faf47ee7565ddd4ca
Bohdan Juříček <juriczech@Bohdan-MacBook-Air.local>
2019-02-20 11:04:25 +0100

    names

--- database/migrations/2019_02_20_095417_add_nullable.php
+++ database/migrations/2019_02_20_095417_add_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNullable extends Migration
+class AddNullableAnswers extends Migration
 {
     /**
      * Run the migrations.
