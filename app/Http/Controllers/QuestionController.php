ac740aa0b8e253de07b8f3e40c7fa6046a3a1442
LukasMicka <lukas.micka@seznam.cz>
2019-02-20 12:05:17 +0100

    added modified QuestionController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class QuestionController extends Controller
 {
     //
     public function index()
     {
-        return 'This is the list of questions';
+        $all_questions = DB::table("questions")->orderBy("created_at", "desc")->get();
+
+        dd($all_questions);
     }
     public function show()
     {
-        return 'This is a detail of a question';
+        $question = DB::table("questions")->where('id', 1)->first();
+        
+        $answers_to_q_1 = DB::table("answers")->where('question_id', 1)->oldest()->get();
+    
+        dd($answers_to_q_1);
     }
 }
